10dfa34aef31958d87654175e81455421b4bf14e
Lokesh12906 <lokesh12906@gmail.com>
2026-08-04 01:21:15 +0530

    Update tb2_mips.v

--- tbs/tb2_mips.v
+++ tbs/tb2_mips.v
@@ -19,23 +19,17 @@ module tb2;
       dut.REG[k] = k;
 
     dut.MEM[0] = 32'h28010078; // ADDI R1, R0, 120
-    dut.MEM[1] = 32'h0c631800; // OR   R3, R3, R3  (dummy)
-    dut.MEM[2] = 32'h20220000; // LW   R2, 0(R1)
-    dut.MEM[3] = 32'h0c631800; // OR   R3, R3, R3  (dummy)
-    dut.MEM[4] = 32'h2842002d; // ADDI R2, R2, 45
-    dut.MEM[5] = 32'h0c631800; // OR   R3, R3, R3  (dummy)
-    dut.MEM[6] = 32'h24220001; // SW   R2, 1(R1)
-    dut.MEM[7] = 32'hfc000000; // HLT
-
-    dut.MEM[120] = 85;
+    dut.MEM[1] = 32'h2802002d; // ADDI R2, R0, 45
+    dut.MEM[2] = 32'h24220001; // SW   R2, 1(R1)
+    dut.MEM[3] = 32'hfc000000; // HLT
 
     dut.PC = 0;
     dut.HALTED = 0;
     dut.TAKEN_BRANCH = 0;
 
-    #500
-    $display("MEM[120]: %4d \nMEM[121]: %4d",
-              dut.MEM[120], dut.MEM[121]);
+    #500;
+    $display("MEM[120]: %0d", dut.MEM[120]);
+    $display("MEM[121]: %0d", dut.MEM[121]);
   end
 
   initial 
